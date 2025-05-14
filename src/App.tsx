9082744e7dc1368b2a76e7656f4b910179580e48
aboudev22 <aboubakarjay2@gmail.com>
2025-05-14 15:14:24 +0100

    May 14

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,31 @@
-import { useMemo, useState } from "react";
+import React, { useMemo, useState } from "react";
+
+const Fruits = React.memo(({ fruits }: { fruits: string[] }) => {
+  console.log("Rendue de la liste");
+  return (
+    <ul className="w-fit flex gap-2">
+      {fruits.map((fruit, index) => (
+        <li key={index} className="p-2 w-fit bg-violet-200 text-black">
+          {fruit}
+        </li>
+      ))}
+    </ul>
+  );
+});
 
 export default function App() {
   const [count, setCount] = useState(0);
-  const expensiveCalculation = (num: number) => {
-    console.log("Calcul en cours...");
-    let result = 0;
-    for (let i = 0; i < 1000000000; i++) {
-      result += num;
-    }
-    return result;
-  };
-  const memoizedValue = useMemo(() => expensiveCalculation(count), [count]);
-
+  const items = useMemo(() => ["mangue", "patate", "poivre"], []);
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <section className="w-sm bg-blue-200 rounded-2xl p-5 flex flex-col items-center justify-center">
-        <h1 className="">Count: {count}</h1>
-        <button
-          className="p-2 rounded-md bg-black text-white font-bold cursor-pointer"
-          onClick={() => setCount(count + 1)}
-        >
-          Incrémenter
-        </button>
-        <p>Résultat : {memoizedValue}</p>
-      </section>
+    <div className="w-screen h-screen flex-col gap-4 flex justify-center items-center">
+      <p className="text-red-500 bg-black p-4 font-bold">{count}</p>
+      <button
+        onClick={() => setCount(count + 1)}
+        className="p-2 rounded-md text-white bg-black focus:bg-black/90 cursor-pointer"
+      >
+        Incremente
+      </button>
+      <Fruits fruits={items} />
     </div>
   );
 }
